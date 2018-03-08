c0f6f37ab55cfcb805bb8e6a3329141a5a027600
Chris Mazzochi <cmazzochi81@gmail.com>
2018-03-08 17:11:06 -0500

    better

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,14 @@
 <?php
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "xxxxxx");
-defined('DB_PASS')   ? null : define("DB_PASS", "xxxxxx");
-defined('DB_NAME')   ? null : define("DB_NAME", "mazzo_php_app");
+// defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+// defined('DB_USER')   ? null : define("DB_USER", "xxxxxx");
+// defined('DB_PASS')   ? null : define("DB_PASS", "xxxxxx");
+// defined('DB_NAME')   ? null : define("DB_NAME", "mazzo_php_app");
+$mysql_url = parse_url($_ENV["DATABASE_URL"]);
+$db = substr($mysql_url['path'], 1);
+define('DB_NAME', $db);
+define('DB_USER', $mysql_url['user']);
+define('DB_PASSWORD', $mysql_url['pass']);
+define('DB_SERVER', $mysql_url['host'] . ":" . $mysql_url['port']);
+
+
 ?>
