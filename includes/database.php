7ce2fb38639d7fe88aab15a56d14f214778892f5
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-01 17:45:17 -0700

    improving

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,12 @@
 <?php 
-
-//require_once("db_credentials.php");
-class MySQLDatabase{
-
-    define("DB_SERVER", "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("DB_SERVER", "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
 define("DB_USER", "ssfamb89it7fgn5r");
 define("DB_PASS", "ywhs2p5f40ia9rmq");
 define("DB_NAME", "bzvt9enln94pbwa7");
+//require_once("db_credentials.php");
+class MySQLDatabase{
+
+ 
     private $connection;
     
     function __construct(){
