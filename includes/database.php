2933505996e31e2049008705f27f8b0af60ed72e
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-01 17:50:23 -0700

    yes

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,7 @@
 <?php 
-define("DB_SERVER", "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
-define("DB_USER", "ssfamb89it7fgn5r");
-define("DB_PASS", "ywhs2p5f40ia9rmq");
-define("DB_NAME", "bzvt9enln94pbwa7");
-//require_once("db_credentials.php");
+require_once("db_credentials.php");
 class MySQLDatabase{
 
- 
     private $connection;
     
     function __construct(){
--- includes/db_credentials.php
+++ includes/db_credentials.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_SERVER", "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+//define("DB_SERVER", "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
 define("DB_USER", "ssfamb89it7fgn5r");
 define("DB_PASS", "ywhs2p5f40ia9rmq");
 define("DB_NAME", "bzvt9enln94pbwa7");
