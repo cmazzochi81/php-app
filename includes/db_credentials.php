352b5355b904c87a8ef207451db31349eeb1e2fc
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-02 12:35:23 -0700

    improving

--- includes/db_credentials.php
+++ includes/db_credentials.php
@@ -7,10 +7,9 @@
 // $dbname = ltrim($dbparts['path'], '/');
 
 define("$url", getenv('JAWSDB_URL'));
-define("$dbparts", parse_url($url))
+define("$dbparts", parse_url($url));
 define("DB_SERVER", $dbparts['host']);
 define("DB_USER", $dbparts['user']);
 define("DB_PASS", $dbparts['pass']);
-define("DB_NAME", ltrim($dbparts['path'], '/');
-
+define("DB_NAME", ltrim($dbparts['path'], '/'));
 ?>
