79260fe140471ef5df0bb292d51dee5adc0b2729
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-03 10:46:02 -0700

    improving

--- public/index.php
+++ public/index.php
@@ -39,10 +39,15 @@ $message = "";
     <div id="linksDiv" class="clearfix">
         <ul class="clearfix">
             <li class="links linkHome"><a class="active" href="index.php">Home</a></li>
+
             <li class="links linkGallery"><a href="gallery.php">Gallery</a></li>
-            <li class="linkJava"><a href="http://play-auction-app.osc-fr1.scalingo.io/">Java App<i class="fa fa-angle-right"></i></a></li>
-            <li class="linkAngular"><a href="https://mazzo-angular-app.herokuapp.com/">Angular App<i class="fa fa-angle-right"></i></a></li>
-            <li class="linkNet"><a href="https://mazzo-react-test.herokuapp.com/">React Demo<i class="fa fa-angle-right"></i></a></li>
+
+            <li class="links linkJava"><a href="http://play-auction-app.osc-fr1.scalingo.io/">Java App<i class="fa fa-angle-right"></i></a></li>
+
+            <li class="links linkAngular"><a href="https://mazzo-angular-app.herokuapp.com/">Angular App<i class="fa fa-angle-right"></i></a></li>
+
+            <li class="links linkNet"><a href="https://mazzo-react-test.herokuapp.com/">React Demo<i class="fa fa-angle-right"></i></a></li>
+
             <li class="linkNet"><a href="https://vue-mazzo.herokuapp.com/">Vue Demo<i class="fa fa-angle-right"></i></a></li>
 
             <?php
