9ed52a5d51f5d7f38bae4f850263d61b11d03b7e
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-04 10:44:01 -0700

    improving

--- public/admin/list_photos.php
+++ public/admin/list_photos.php
@@ -1,9 +1,9 @@
-<?php require_once("../../includes/database_object.php");?>
 <?php require_once("../../includes/initialize.php"); ?>
 <?php if (!$session->is_logged_in()) {redirect_to("login.php");} ?>
 
 <?php
     $photos = Photograph::find_all();
+    var_dump($photos);
     $message = "";
 ?>
 <?php include_layout_template('admin_header.php'); ?>
