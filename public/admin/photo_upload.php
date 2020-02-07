2bdb98119b873568dc84f8ad2cb3925955461f3b
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-07 11:58:00 -0700

    improving

--- public/admin/photo_upload.php
+++ public/admin/photo_upload.php
@@ -41,7 +41,7 @@ if (isset($_POST['submit'])) {
 <form action="photo_upload.php" enctype="multipart/form-data" method="POST">
     <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size; ?>" />
     <p><input type="file" name="file_upload" /></p>
-    <p style="font-color:#000;">Caption: <input type="text" name="caption" value="" /></p>
+    <p style="color:#000;">Caption: <input type="text" name="caption" value="" /></p>
     <input type="submit" name="submit" value="Upload" />
 </form>
 
