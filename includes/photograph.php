590661946c6feb4c2a8e05be29cdc684e71d41e8
Chris Mazzochi <cmazzochi81@gmail.com>
2020-02-06 17:59:49 -0700

    improving

--- includes/photograph.php
+++ includes/photograph.php
@@ -2,13 +2,7 @@
 require_once(LIB_PATH . DS . 'database.php');
 require_once(LIB_PATH . DS . 'initialize.php');
 require(SITE_ROOT . DS . 'vendor/autoload.php');
-$s3 = new Aws\S3\S3Client([
-    'version'  => '2006-03-01',
-    'region'   => 'us-east-1',
-]);
 
-$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
-var_dump($bucket);
 class Photograph extends DatabaseObject {
 
     protected static $table_name = "photographs";
@@ -64,6 +58,14 @@ class Photograph extends DatabaseObject {
   }
 
     public function save() {
+
+        $s3 = new Aws\S3\S3Client([
+        'version'  => '2006-03-01',
+        'region'   => 'us-east-1',
+        ]);
+
+        $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+        var_dump($bucket);
         // A new record won't have an id yet.
         if (isset($this->id)) {
           //if(!empty($this->id)){
@@ -91,7 +93,7 @@ class Photograph extends DatabaseObject {
             // Determine the target_path
 
              // $target_path = SITE_ROOT . DS . 'public' . DS . $this->upload_dir . DS . $this->filename;
-            $target_path = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+            $target_path = $this->s3->upload($this->bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
 
             // $target_path = SITE_ROOT . DS . 'public' . DS . $this->upload_dir . DS . $this->filename;
             // $target_path = SITE_ROOT . DS . $this->upload_dir . DS . $this->filename;
